f29e00ac04f917b9fbcb4fa667b1fc3231894527
Jason <jjrock2@illinois.edu>
2013-12-04 14:37:56 -0600

    adding a reader for a concatenated det file.

---
+++ KalmanFilter/readDet.m
@@ -0,0 +1,30 @@
+classdef readDet < handle
+    %READDET Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        line_num;
+        split_s;
+    end
+    
+    methods
+        function obj = readDet(filename)
+            s = fileread(filename);
+            obj.split_s = strsplit(s,'\n');
+            obj.line_num = 1;
+        end
+        
+        function [val] = readline(obj)
+            if(regexp(obj.split_s{obj.line_num+1}, 'loading'))
+                obj.line_num = obj.line_num+1;
+                val = [];
+                return;
+            end
+            s = obj.split_s{obj.line_num+1};
+            obj.line_num = obj.line_num+2;
+            val = str2num(s);
+        end
+    end
+    
+end
+
