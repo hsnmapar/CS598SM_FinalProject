7b9d6da2e6fe7a5e7cbdfbbd5cca3f7d898353f3
Jason <jjrock2@illinois.edu>
2013-12-04 22:47:40 -0600

    clean up generation of data

--- KalmanFilter/runKalman.m
+++ KalmanFilter/runKalman.m
@@ -1,25 +1,36 @@
+
+%Loads the tracked tags locations and kalman filters them.  Tags are then
+%subsampled randomly and evenly and then kalman filter applied.  Cost is
+%the error rate for the kalman filter.
+
 [filename, pathname] = uigetfile({'*.txt'});
 det = readDet([pathname '/' filename]);
 val_original = det.readAll();
 val_gt.all = val_original;
 val_kalman.all = ExtendedKalmanFilterSize(val_original);
 Q = val_kalman.all(1:3,:)'-val_gt.all;
-cost.all = mean(Q(~any(isnan(Q),2),:));
+val_kalman.all = val_kalman.all(1:3,:)';
+
+cost.all = mean(abs(Q(~any(isnan(Q),2),:)));
 for i = 1:10
     val_new = NaN(size(val_original));
     val_new(1:i:end,:) = val_original(1:i:end,:);
     val_gt.subsample_even{i} = val_new;
     val_kalman.subsample_even{i} = ExtendedKalmanFilterSize(val_new);
     Q = val_kalman.subsample_even{i}(1:3,:)'-val_gt.all;
-    cost.subsample_even{i} = mean(Q(~any(isnan(Q),2),:));
+    cost.subsample_even{i} = mean(abs(Q(~any(isnan(Q),2),:)));
+    val_kalman.subsample_even{i} = val_kalman.subsample_even{i}(1:3,:)';
 end
 
 for i = 1:10
-    sel = randsample(size(val_original,1),floor(size(val_original,1)/8));
-    val_new = val_original;
-    val_new(sel,:) = NaN;
-    val_gt.subsample_random{i} = val_new;
-    val_kalman.subsample_random{i} = ExtendedKalmanFilterSize(val_new);
-    Q = val_kalman.subsample_random{i}(1:3,:)'-val_gt.all;
-    cost.subsample_random{i} = mean(Q(~any(isnan(Q),2),:));
+    for j = 1:10
+        sel = randsample(size(val_original,1),floor(size(val_original,1)*(j-1)/j));
+        val_new = val_original;
+        val_new(sel,:) = NaN;
+        val_gt.subsample_random{i} = val_new;
+        val_kalman.subsample_random{i,j} = ExtendedKalmanFilterSize(val_new);
+        Q = val_kalman.subsample_random{i,j}(1:3,:)'-val_gt.all;
+        cost.subsample_random{i,j} = mean(abs(Q(~any(isnan(Q),2),:)));
+        val_kalman.subsample_random{i,j} = val_kalman.subsample_random{i,j}(1:3,:)';
+    end
 end
