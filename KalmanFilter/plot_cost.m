e0e1eecebabcd2e09b57a75bfbd879e2d61b91ef
Jason <jjrock2@illinois.edu>
2013-12-04 22:47:17 -0600

    adding a bar plot for the cost (error) of the kalman filter relative to the ground truth detections.

---
+++ KalmanFilter/plot_cost.m
@@ -0,0 +1,15 @@
+function [ C_concat ] = plot_cost( cost )
+
+C_concat = [cost.all,NaN,NaN,NaN];
+
+for i = 2:10
+    C_concat = [C_concat; cost.subsample_even{i},mean(cell2mat(cost.subsample_random(:,i)))];
+end
+figure, hold on;
+set(gca,'yscale','log');
+bar(C_concat);
+set(gca,'XTick',[1 2:10])
+set(gca,'XTickLabel',{'all';'1/2';'1/3';'1/4';'1/5';'1/6';'1/7';'1/8';'1/9';'1/10'})
+legend(gca,'xerror','yerror','sizeerror','xerror random','yerror random','sizeerror random');
+end
+
