68a9c439796065e19b9ed12b8bb66c931ee40adf
Kevin Shih <kevin.j.shih@gmail.com>
2013-12-04 22:35:15 -0600

    tracking tag with kalman data

--- track_tag.m
+++ track_tag.m
@@ -1,4 +1,6 @@
 function[] = track_tag(detection_dir) 
+% load kalman data
+load(fullfile(detection_dir, 'kalman_data.mat'));
 
 N=300;
 
@@ -15,31 +17,52 @@ for i = 1:length(cached_dets)
     end
 end
 
-%% isolate relevant tracked features in the first frame:
 [x,y, val] = klt_read_featuretable(fullfile(detection_dir, sprintf('Out_B5_N%d_R', N), 'features.txt'));
-
-first_det = cached_dets{first}.det;
-hull = round([first_det(4:5); first_det(6:7); first_det(8:9); first_det(10:11)]);
-
-relevant_pnts = inhull([round(x(:,first)), round(y(:,first))], hull);
-
-[rad cntr] = get_circle([x(relevant_pnts,first), y(relevant_pnts,first)]);
-im = load_current_image(detection_dir, cached_dets{first}.name);
-im = imshow(im);
-hold on;
-plot_circle(cntr(1), cntr(2), rad);
-drawnow;
-% now follow those points until you've lost them
-
-for i = first_det+1:length(cached_dets)
-im = load_current_image(detection_dir, cached_dets{i}.name);
-
-im = imshow(im);
-hold on;
-[rad cntr] = get_circle([x(relevant_pnts,i), y(relevant_pnts,i)]);
-plot_circle(cntr(1), cntr(2), rad);
-drawnow;
+% assume first image has detection
+for i = 1:length(cached_dets)
+    im = load_current_image(detection_dir, cached_dets{i}.name);
+    imshow(im);
+    hold on;
+    [rad, relevant_pnts] = expand_circle(val_kalman.all(i,3), val_kalman.all(i,1:2), [x(:,i), y(:,i)]);
+    if i == 1 % first frame, just draw the 
+        det = cached_dets{i}.det;
+        hull = round([det(4:5); det(6:7); det(8:9); det(10:11)]);
+        plot(hull(:,1), hull(:,2))
+        keyboard;
+    else
+    end
+    plot_circle(round(val_kalman.all(i,1)), round(val_kalman.all(i,2)), round(val_kalman.all(i,3)));
+    drawnow;
+    hold off;
 end
+
+
+
+%% isolate relevant tracked features in the first frame:
+% [x,y, val] = klt_read_featuretable(fullfile(detection_dir, sprintf('Out_B5_N%d_R', N), 'features.txt'));
+
+% first_det = cached_dets{first}.det;
+% hull = round([first_det(4:5); first_det(6:7); first_det(8:9); first_det(10:11)]);
+
+% relevant_pnts = inhull([round(x(:,first)), round(y(:,first))], hull);
+
+% [rad cntr] = get_circle([x(relevant_pnts,first), y(relevant_pnts,first)]);
+% im = load_current_image(detection_dir, cached_dets{first}.name);
+% im = imshow(im);
+% hold on;
+% plot_circle(cntr(1), cntr(2), rad);
+% drawnow;
+% % now follow those points until you've lost them
+
+% for i = first_det+1:length(cached_dets)
+% im = load_current_image(detection_dir, cached_dets{i}.name);
+
+% im = imshow(im);
+% hold on;
+% [rad cntr] = get_circle([x(relevant_pnts,i), y(relevant_pnts,i)]);
+% plot_circle(cntr(1), cntr(2), rad);
+% drawnow;
+% end
 
 keyboard;
 
@@ -65,7 +88,7 @@ end
 function[H] = compute_homography(xy_old, xy_new)
 % [x y; x y; x y; ...]
 num_pnts = size(xy_old,1);
-A = zeros(num_pnts.2, 8);
+A = zeros(num_pnts*2, 8);
 for i = 1:num_pnts
     A(2*i-1, 1:3) = [xy_old(i,:), 1];
     A(2*i-1, 7:8) = -[xy_new(1).*xy_old];
@@ -95,7 +118,7 @@ xy_new = tmp';
 
 function[im] = load_current_image(detection_dir, fname)
 im = imread(fullfile(detection_dir,[fname,'.pnm']));
-
+ 
 function plot_circle(x,y,r)
 %x and y are the coordinates of the center of the circle
 %r is the radius of the circle
