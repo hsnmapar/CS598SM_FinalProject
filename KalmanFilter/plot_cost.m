d1ad26bf063ceabd52026c1457ead27cc5ac10bf
Jason <jjrock2@illinois.edu>
2013-12-08 19:37:44 -0600

    working implementation of stuff

--- KalmanFilter/plot_cost.m
+++ KalmanFilter/plot_cost.m
@@ -1,12 +1,13 @@
-function [ C_concat ] = plot_cost( cost )
+function [ C_concat ] = plot_cost( cost, fig)
 
 C_concat = [cost.all,NaN,NaN,NaN];
 
 for i = 2:10
     C_concat = [C_concat; cost.subsample_even{i},mean(cell2mat(cost.subsample_random(:,i)))];
 end
-figure, hold on;
+%figure, hold on;
 set(gca,'yscale','log');
+hold on;
 bar(C_concat);
 set(gca,'XTick',[1 2:10])
 set(gca,'XTickLabel',{'all';'1/2';'1/3';'1/4';'1/5';'1/6';'1/7';'1/8';'1/9';'1/10'})
