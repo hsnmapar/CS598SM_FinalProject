1b71b5d0553fb4024a3a2de27d8d2b9963545955
Jason <jjrock2@illinois.edu>
2013-12-09 20:42:17 -0600

    updates plot error and error plots in the final writeup dir

--- plotError.m
+++ plotError.m
@@ -11,7 +11,16 @@ for i = [2,5,10]
     Q(:,end+1) = mean(R');
 end
 
-plot(Q);
+hold on;
+plot(Q(:,1),'-k');
+plot(Q(:,2),'-r');
+plot(Q(:,3),'--r');
+plot(Q(:,4),'-g');
+plot(Q(:,5),'--g');
+plot(Q(:,6),'-b');
+plot(Q(:,7),'--b');
+axis([0,size(Q,1),0,100]);
+
 %set(gca, 'ymax', 100);
 %set(gca,'yscale','log')
 legend(gca,'all','1/2 even', '1/2 random', '1/5 even' ,'1/5 random', ...
